1fc4ef34c62c35f3aaeb3a493963e1446e420143
Le Ky Vong <lekyvong1303@gmail.com>
2026-05-29 17:49:10 +0700

    update Front-end

--- Front-end/my-app/src/materialReceipts/materialReceipts.js
+++ Front-end/my-app/src/materialReceipts/materialReceipts.js
@@ -1747,7 +1747,6 @@ export const MaterialReceipts = () => {
             columns={columns}
             data={filteredData}
             bodyCellClassName="!py-2 sm:!py-3"
-            rowClassName={(row) => isPurchasingEmployee && getReceiptStatus(row) === "2" ? "!bg-pink-100 hover:!bg-pink-120" : ""}
             renderExpansion={(row) => (
               <div className="py-4 px-4 sm:pl-24 sm:pr-6 bg-blue-50/30 border-b border-gray-100 relative">
                 <div className="grid grid-cols-1 lg:grid-cols-4 gap-y-6 gap-x-8 text-sm">
